13edf7d99cf85504d3008b01408bc1167ca99b31
remisiracusa <remi.siracusa@orange.fr>
2019-05-10 17:58:33 +0200

    maj IA java

--- Prolog/src/Moteur_IA.java
+++ Prolog/src/Moteur_IA.java
@@ -25,151 +25,6 @@ public class Moteur_IA {
 	static boolean cont;
 
 	public static void main(String[] args) {
-		TCoup test = jasper2();
-	}
-	
-	public static TCoup jasper2() {
-		TCoup coup = new TCoup();
-		//String saisie = new String("yokai([11,9,10,9,11,0,0,0,0,0,0,7,7,7,0,0,1,1,1,0,0,0,0,0,0,5,3,4,3,5],top,Sol).");
-		String saisie = new String("yokai2([11,9,10,9,11,0,0,0,0,0,0,7,7,7,0,0,1,1,1,0,0,0,0,0,0,5,3,4,3,5],top,Piece,Ind,NewInd,CarteCapturee).");
-		//String saisie = new String("yokai2([11,9,10,9,11,0,0,0,0,0,0,7,7,7,0,0,1,1,1,0,0,0,0,0,0,5,3,4,3,5],top,Sol).");
-		
-		SICStus sp = null;
-
-		try {
-
-			// Creation d'un object SICStus
-			sp = new SICStus();
-
-			// Chargement d'un fichier prolog .pl
-			sp.load("./src/projet_ia/yokai.pl");
-			
-		}
-		// exception déclanchée par SICStus lors de la création de l'objet sp
-		catch (SPException e) {
-			System.err.println("Exception SICStus Prolog : " + e);
-			e.printStackTrace();
-			System.exit(-2);
-		}
-
-		// lecture au clavier d'une requète Prolog
-		//System.out.print("| ?- ");
-		//saisie = saisieClavier();
-
-		// boucle pour saisir les informations
-		//while (! saisie.equals("halt.")) {
-
-			// HashMap utilisé pour stocker les solutions
-			HashMap results = new HashMap();
-			SPTerm piece = new SPTerm(sp);
-			SPTerm ind = new SPTerm(sp);
-			SPTerm newInd = new SPTerm(sp);
-			SPTerm carteCapturee = new SPTerm(sp);
-			try {
-
-				// Creation d'une requete (Query) Sicstus
-				//   - en fonction de la saisie de l'utilisateur
-				//   - instanciera results avec les résultats de la requète
-				Query qu = sp.openQuery(saisie,results);
-
-				// parcours des solutions
-				boolean moreSols = qu.nextSolution();
-
-				// on ne boucle que si la liste des instanciations de variables est non vide
-				boolean continuer = !(results.isEmpty());
-
-				while (moreSols && continuer) {
-					//Voir pour deposer
-					coup.setCodeRep(CodeRep.DEPLACER);
-					// chaque solution est sockée dans un HashMap
-					// sous la forme : VariableProlog -> Valeur
-					//System.out.println(results + " ? ");
-					piece = (SPTerm) results.get("Piece");
-					ind = (SPTerm) results.get("Ind");
-					newInd = (SPTerm) results.get("NewInd");
-					carteCapturee = (SPTerm) results.get("CarteCapturee");
-					
-					switch((int) piece.getInteger()){
-						case 1 :
-							coup.setPieceType(Type.KODAMA);
-							break;
-						case 2 :
-							coup.setPieceType(Type.KODAMA_SAMOURAI);
-							break;
-						case 3 :
-							coup.setPieceType(Type.KIRIN);
-							break;
-						case 4 :
-							coup.setPieceType(Type.KOROPOKKURU);
-							break;
-						case 5 :
-							coup.setPieceType(Type.ONI);
-							break;
-						case 6 :
-							coup.setPieceType(Type.SUPER_ONI);
-							break;
-						case 7 :
-							coup.setPieceType(Type.KODAMA);
-							break;
-						case 8 :
-							coup.setPieceType(Type.KODAMA_SAMOURAI);
-							break;
-						case 9 :
-							coup.setPieceType(Type.KIRIN);
-							break;
-						case 10 :
-							coup.setPieceType(Type.KOROPOKKURU);
-							break;
-						case 11 :
-							coup.setPieceType(Type.ONI);
-							break;
-						case 12 :
-							coup.setPieceType(Type.SUPER_ONI);
-							break;
-						default:
-							break;
-					}
-					
-					if(((int) carteCapturee.getInteger()) == 0) {
-						coup.setEstCapt(false);
-					}else {
-						coup.setEstCapt(true);
-					}
-					
-					// demande à l'utilisateur de continuer ...
-					//saisie = saisieClavier();
-					if (saisie.equals(";")) {
-						// solution suivante --> results contient la nouvelle solution
-						moreSols = qu.nextSolution();
-					}
-					else {
-						continuer = false;
-					}
-				}
-
-				// fermeture de la requète
-				System.err.println("Fermeture requete");
-				qu.close();
-
-			}
-			catch (SPException e) {
-				System.err.println("Exception prolog\n" + e);
-			}
-			// autres exceptions levées par l'utilisation du Query.nextSolution()
-			catch (Exception e) {
-				System.err.println("Other exception : " + e);
-			}
-
-			//System.out.print("| ?- ");
-			// lecture au clavier
-			//saisie = saisieClavier();        
-		//}
-		System.out.println("End of jSicstus");
-		System.out.println("Bye bye");
-		return coup;
-	}
-	
-	public static void IA() {
 		ServerSocket srv; //Socket de connexion
 		Socket sockComm; // Socket de communication
 		TPartie TPartie = new TPartie();
@@ -208,7 +63,7 @@ public class Moteur_IA {
 					if(tour) {
 
 						//Trouver un coup avec Prolog + gerer si timeout recu -> numPartie++; break;
-
+						TCoup coup = coupProlog();
 						//Envoie coup
 						switch(TCoup.getCodeRep()) {
 						case DEPLACER :
@@ -225,136 +80,131 @@ public class Moteur_IA {
 						}
 
 						if(TCoup.getCodeRep() != CodeRep.AUCUN) {
-							switch(dis.readInt()) {
-							case 0 :
-								TCoup.setPieceSens(Sens.NORD);
+							switch(sens) {
+							case NORD :
+								dos.writeInt(0);
 								break;
-							case 1 :
-								TCoup.setPieceSens(Sens.SUD);
-								break;
-							default :
-								break;
-							}
-
-							switch(dis.readInt()) {
-							case 0 :
-								TCoup.setPieceType(Type.KODAMA);
-								break;
-							case 1 :
-								TCoup.setPieceType(Type.KODAMA_SAMOURAI);
-								break;
-							case 2 :
-								TCoup.setPieceType(Type.KIRIN);
-								break;
-							case 3 :
-								TCoup.setPieceType(Type.KOROPOKKURU);
-								break;
-							case 4 :
-								TCoup.setPieceType(Type.ONI);
-								break;
-							case 5 :
-								TCoup.setPieceType(Type.SUPER_ONI);
+							case SUD :
+								dos.writeInt(1);
 								break;
 							default :
 								break;
 							}
 
-							switch(dis.readInt()) {
-							case 0 :
-								TCoup.setTlgDep(Ligne.UN);
+							switch(coup.getPieceType()) {
+							case KODAMA :
+								dos.writeInt(0);
 								break;
-							case 1 :
-								TCoup.setTlgDep(Ligne.DEUX);
+							case KODAMA_SAMOURAI :
+								dos.writeInt(1);
 								break;
-							case 2 :
-								TCoup.setTlgDep(Ligne.TROIS);
+							case KIRIN :
+								dos.writeInt(2);
 								break;
-							case 3 :
-								TCoup.setTlgDep(Ligne.QUATRE);
+							case KOROPOKKURU :
+								dos.writeInt(3);
 								break;
-							case 4 :
-								TCoup.setTlgDep(Ligne.CINQ);
+							case ONI :
+								dos.writeInt(4);
 								break;
-							case 5 :
-								TCoup.setTlgDep(Ligne.SIX);
+							case SUPER_ONI :
+								dos.writeInt(5);
 								break;
 							default :
 								break;
 							}
 
-							switch(dis.readInt()) {
-							case 0 :
-								TCoup.setTcolDep(Colonne.A);
+							switch(coup.getTlgDep()) {
+							case UN :
+								dos.writeInt(0);
 								break;
-							case 1 :
-								TCoup.setTcolDep(Colonne.B);
+							case DEUX :
+								dos.writeInt(1);
 								break;
-							case 2 :
-								TCoup.setTcolDep(Colonne.C);
+							case TROIS :
+								dos.writeInt(2);
 								break;
-							case 3 :
-								TCoup.setTcolDep(Colonne.D);
+							case QUATRE :
+								dos.writeInt(3);
 								break;
-							case 4 :
-								TCoup.setTcolDep(Colonne.E);
+							case CINQ :
+								dos.writeInt(4);
+								break;
+							case SIX :
+								dos.writeInt(5);
+								break;
+							default :
+								break;
+							}
+
+							switch(coup.getTcolDep()) {
+							case A :
+								dos.writeInt(0);
+								break;
+							case B :
+								dos.writeInt(1);
+								break;
+							case C :
+								dos.writeInt(2);
+								break;
+							case D :
+								dos.writeInt(3);
+								break;
+							case E :
+								dos.writeInt(4);
 								break;
 							default :
 								break;
 							}
 
 							if(TCoup.getCodeRep() == CodeRep.DEPLACER) {
-								switch(dis.readInt()) {
-								case 0 :
-									TCoup.setTlgArr(Ligne.UN);
+								switch(coup.getTlgArr()) {
+								case UN :
+									dos.writeInt(0);
 									break;
-								case 1 :
-									TCoup.setTlgArr(Ligne.DEUX);
+								case DEUX :
+									dos.writeInt(1);
 									break;
-								case 2 :
-									TCoup.setTlgArr(Ligne.TROIS);
+								case TROIS :
+									dos.writeInt(2);
 									break;
-								case 3 :
-									TCoup.setTlgArr(Ligne.QUATRE);
+								case QUATRE :
+									dos.writeInt(3);
 									break;
-								case 4 :
-									TCoup.setTlgArr(Ligne.CINQ);
+								case CINQ :
+									dos.writeInt(4);
 									break;
-								case 5 :
-									TCoup.setTlgArr(Ligne.SIX);
-									break;
-								default :
-									break;
-								}
-
-								switch(dis.readInt()) {
-								case 0 :
-									TCoup.setTcolArr(Colonne.A);
-									break;
-								case 1 :
-									TCoup.setTcolArr(Colonne.B);
-									break;
-								case 2 :
-									TCoup.setTcolArr(Colonne.C);
-									break;
-								case 3 :
-									TCoup.setTcolArr(Colonne.D);
-									break;
-								case 4 :
-									TCoup.setTcolArr(Colonne.E);
+								case SIX :
+									dos.writeInt(5);
 									break;
 								default :
 									break;
 								}
 
-								switch(dis.readInt()) {
-								case 0 :
-									TCoup.setEstCapt(false);
+								switch(coup.getTcolArr()) {
+								case A :
+									dos.writeInt(0);
 									break;
-								case 1 :
-									TCoup.setEstCapt(true);
+								case B :
+									dos.writeInt(1);
+									break;
+								case C :
+									dos.writeInt(2);
+									break;
+								case D :
+									dos.writeInt(3);
+									break;
+								case E :
+									dos.writeInt(4);
 									break;
 								default :
 									break;
+								}
+
+								if(coup.isEstCapt()) {
+									dos.writeInt(1);
+								}else {
+									dos.writeInt(0);
 								}
 								//Deplacer sur grille
 							}else {
@@ -538,12 +388,12 @@ public class Moteur_IA {
 		}catch(IOException e){
 			e.printStackTrace();
 		}
-			
-	}
 
-	public static void jasper() {
-		
-		String saisie = new String("");
+	}
+	
+	public static TCoup coupProlog() {
+		TCoup coup = new TCoup();
+		String saisie = new String("yokai2([11,9,10,9,11,0,0,0,0,0,0,7,7,7,0,0,1,1,1,0,0,0,0,0,0,5,3,4,3,5],top,Piece,Ind,NewInd,CarteCapturee).");
 
 		SICStus sp = null;
 
@@ -564,92 +414,119 @@ public class Moteur_IA {
 		}
 
 		// lecture au clavier d'une requète Prolog
-		System.out.print("| ?- ");
-		saisie = saisieClavier();
+		//System.out.print("| ?- ");
+		//saisie = saisieClavier();
 
 		// boucle pour saisir les informations
-		while (! saisie.equals("halt.")) {
+		//while (! saisie.equals("halt.")) {
 
-			// HashMap utilisé pour stocker les solutions
-			HashMap results = new HashMap();
+		// HashMap utilisé pour stocker les solutions
+		HashMap results = new HashMap();
+		SPTerm piece = new SPTerm(sp);
+		SPTerm ind = new SPTerm(sp);
+		SPTerm newInd = new SPTerm(sp);
+		SPTerm carteCapturee = new SPTerm(sp);
+		try {
 
-			try {
+			// Creation d'une requete (Query) Sicstus
+			//   - en fonction de la saisie de l'utilisateur
+			//   - instanciera results avec les résultats de la requète
+			Query qu = sp.openQuery(saisie,results);
 
-				// Creation d'une requete (Query) Sicstus
-				//   - en fonction de la saisie de l'utilisateur
-				//   - instanciera results avec les résultats de la requète
-				Query qu = sp.openQuery(saisie,results);
+			// parcours des solutions
+			boolean moreSols = qu.nextSolution();
 
-				// parcours des solutions
-				boolean moreSols = qu.nextSolution();
+			// on ne boucle que si la liste des instanciations de variables est non vide
+			boolean continuer = !(results.isEmpty());
 
-				// on ne boucle que si la liste des instanciations de variables est non vide
-				boolean continuer = !(results.isEmpty());
+			while (moreSols && continuer) {
+				//Voir pour deposer
+				coup.setCodeRep(CodeRep.DEPLACER);
+				// chaque solution est sockée dans un HashMap
+				// sous la forme : VariableProlog -> Valeur
+				//System.out.println(results + " ? ");
+				piece = (SPTerm) results.get("Piece");
+				ind = (SPTerm) results.get("Ind");
+				newInd = (SPTerm) results.get("NewInd");
+				carteCapturee = (SPTerm) results.get("CarteCapturee");
 
-				while (moreSols && continuer) {
-
-					// chaque solution est sockée dans un HashMap
-					// sous la forme : VariableProlog -> Valeur
-					System.out.print(results + " ? ");
-
-					// demande à l'utilisateur de continuer ...
-					saisie = saisieClavier();
-					if (saisie.equals(";")) {
-						// solution suivante --> results contient la nouvelle solution
-						moreSols = qu.nextSolution();
-					}
-					else {
-						continuer = false;
-					}
+				switch((int) piece.getInteger()){
+				case 1 :
+					coup.setPieceType(Type.KODAMA);
+					break;
+				case 2 :
+					coup.setPieceType(Type.KODAMA_SAMOURAI);
+					break;
+				case 3 :
+					coup.setPieceType(Type.KIRIN);
+					break;
+				case 4 :
+					coup.setPieceType(Type.KOROPOKKURU);
+					break;
+				case 5 :
+					coup.setPieceType(Type.ONI);
+					break;
+				case 6 :
+					coup.setPieceType(Type.SUPER_ONI);
+					break;
+				case 7 :
+					coup.setPieceType(Type.KODAMA);
+					break;
+				case 8 :
+					coup.setPieceType(Type.KODAMA_SAMOURAI);
+					break;
+				case 9 :
+					coup.setPieceType(Type.KIRIN);
+					break;
+				case 10 :
+					coup.setPieceType(Type.KOROPOKKURU);
+					break;
+				case 11 :
+					coup.setPieceType(Type.ONI);
+					break;
+				case 12 :
+					coup.setPieceType(Type.SUPER_ONI);
+					break;
+				default:
+					break;
 				}
 
-				if (moreSols) {
-					// soit :
-					//  - il y a encore des solutions et (continuer == false)
-					//  - le prédicat a réussi mais (results.isEmpty() == true)
-					System.out.println("yes");
+				if(((int) carteCapturee.getInteger()) == 0) {
+					coup.setEstCapt(false);
+				}else {
+					coup.setEstCapt(true);
+				}
+
+				// demande à l'utilisateur de continuer ...
+				//saisie = saisieClavier();
+				if (saisie.equals(";")) {
+					// solution suivante --> results contient la nouvelle solution
+					moreSols = qu.nextSolution();
 				}
 				else {
-					// soit :
-					//    - on est à la fin des solutions
-					//    - il n'y a pas de solutions (le while n'a pas été exécuté)
-					System.out.println("no");
+					continuer = false;
 				}
-
-				// fermeture de la requète
-				System.err.println("Fermeture requete");
-				qu.close();
-
-			}
-			catch (SPException e) {
-				System.err.println("Exception prolog\n" + e);
-			}
-			// autres exceptions levées par l'utilisation du Query.nextSolution()
-			catch (Exception e) {
-				System.err.println("Other exception : " + e);
 			}
 
-			System.out.print("| ?- ");
-			// lecture au clavier
-			saisie = saisieClavier();        
+			// fermeture de la requète
+			System.err.println("Fermeture requete");
+			qu.close();
+
 		}
+		catch (SPException e) {
+			System.err.println("Exception prolog\n" + e);
+		}
+		// autres exceptions levées par l'utilisation du Query.nextSolution()
+		catch (Exception e) {
+			System.err.println("Other exception : " + e);
+		}
+
+		//System.out.print("| ?- ");
+		// lecture au clavier
+		//saisie = saisieClavier();        
+		//}
 		System.out.println("End of jSicstus");
 		System.out.println("Bye bye");
+		return coup;
 	}
-	
-	public static String saisieClavier() {
-
-		// declaration du buffer clavier
-		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
-
-		try {
-			return buff.readLine();
-		}
-		catch (IOException e) {
-			System.err.println("IOException " + e);
-			e.printStackTrace();
-			System.exit(-1);
-		}
-		return ("halt.");
-	}	
 }
