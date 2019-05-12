4cbf28a234544f0b8b0dcc4972b727c252a76d97
provalinf <co.merce@hotmail.fr>
2019-05-12 16:56:14 +0200

    makefile etc fix

---
+++ Client/cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/d/ProjetMachineVirtuelle(Yokai no-mori)/git/Projet-IA/Client/client.c" "/mnt/d/ProjetMachineVirtuelle(Yokai no-mori)/git/Projet-IA/Client/cmake-build-debug/CMakeFiles/Client.dir/client.c.o"
+  "/mnt/d/ProjetMachineVirtuelle(Yokai no-mori)/git/Projet-IA/Client/fonctionsTCP/fonctionsTCP.c" "/mnt/d/ProjetMachineVirtuelle(Yokai no-mori)/git/Projet-IA/Client/cmake-build-debug/CMakeFiles/Client.dir/fonctionsTCP/fonctionsTCP.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../fonctionsTCP"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
